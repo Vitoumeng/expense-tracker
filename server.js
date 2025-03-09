c873daf650465f32a9933a7dd3665969199ced17
MENG Vuthvitou <liltou081@gmail.com>
2025-03-09 09:43:18 +0700

    Config cors

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
+
+// middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeader: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
