eb7c0c11d061c3dc6d22025d2ad8c95999fb9ac8
MENG Vuthvitou <liltou081@gmail.com>
2025-03-09 16:30:48 +0700

    Create route for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,47 +3,70 @@ const jwt = require("jsonwebtoken");
 
 // Generate JWT token
 const generateToken = (id) => {
-	return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1h"});
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
 
 // Register user
 exports.registerUser = async (req, res) => {
-	// After create UserSchema
-	const { fullName, email, password, profileImageUrl } = req.body;
-	
-	// Validate: Check for missing fields
-	if(!fullName || !email || !password) {
-		return res.status(400).json({ message: "All fields are required" });
-	}
-	
-	try {
-		// check if email already exists
-		const existingUser = await User.findOne({ email });
-		
-		if(existingUser) {
-			return res.status(400).json({ message: "Email already in use"});
-		}
-		
-		// create the user 
-		const user = await User.create({
-			fullName, email, password, profileImageUrl,
-		});
-		
-		res.status(201).json({ 
-			id: user._id,
-			user,
-			token: generateToken(user._id),
-		});
-	} catch (e) {
-		res
-			.status(500)
-			.json({ message: "Error register user", error: e.message });
-	}
-};
+  // After create UserSchema
+  const { fullName, email, password, profileImageUrl } = req.body;
 
+  // Validate: Check for missing fields
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // check if email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    // create the user
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      profileImageUrl,
+    });
+
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Error register user", error: e.message });
+  }
+};
 
 // Login user
 // exports.loginUser = asyn (req, res) => {}
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Error login user", error: e.message });
+  }
+};
 
 // get User Info
-// exports.getUserInfo= asyn (req, res) => {}
+// exports.getUserInfo= async (req, res) => {}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,15 @@ const express = require("express");
 
 const {
   registerUser,
-  // , loginUser, getUserInfo
+  loginUser,
+  // getUserInfo
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 
-// router.post("/login", loginUser);
+router.post("/login", loginUser);
 
 // router.get("/getUser", protect, getUserInfo);
 
