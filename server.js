e2bbe753e3e587c99668208685df298556cbe8e0
MENG Vuthvitou <liltou081@gmail.com>
2025-03-10 16:11:28 +0700

    Create route for expense copy and past income and change source to category, income to expense

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 const path = require("path");
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
 
 // upload routes
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
