497c1ffb5954d94cce8120ce431d6455b9324e91
MENG Vuthvitou <liltou081@gmail.com>
2025-03-10 15:02:03 +0700

    Create route for download excel

--- routes/incomeRoutes.js
+++ routes/incomeRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.post("/add", protect, addIncome);
 router.get("/get", protect, getAllIncome);
-// router.get("/downloadexcel", protect, downloadIncomeExcel);
+router.get("/downloadexcel", protect, downloadIncomeExcel);
 router.delete("/:id", protect, deleteIncome);
 
 module.exports = router;
