ae1d63f26b94d235289616343992010cfa8edbce
MENG Vuthvitou <liltou081@gmail.com>
2025-03-12 15:17:21 +0700

    Modify totalBalance on dashboardController

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -63,7 +63,7 @@ exports.getDashboardData = async (req, res) => {
 
     res.json({
       totalBalance:
-        (totalIncome[0]?.total || 0) - (totalExpense[0]?.totla || 0),
+        (totalIncome[0]?.total || 0) - (totalExpense[0]?.total || 0),
       totalIncome: totalIncome[0]?.total || 0,
       totalExpense: totalExpense[0]?.total || 0,
       last30daysExpense: {
